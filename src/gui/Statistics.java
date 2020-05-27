b9370fbe04bb41750074295b732f8a8b9c88e190
yichenlin <yichenlin@localhost>
2020-05-27 13:23:19 +0200

    statistic

--- src/gui/Statistics.java
+++ src/gui/Statistics.java
@@ -6,6 +6,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 import controller.BikeCtr;
+import db.BikeInfoDB;
 import db.DataAccessException;
 import model.*;
 
@@ -36,51 +37,42 @@ public class Statistics {
 			
 			//sort by bike sale price
 			Comparator<Bike> bikeSalePriceComparator = (b1, b2)->(
-					b1.getFinalPrice() -b2.getFinalPrice() > 0 ? 1 : 
-						(b1.getFinalPrice()-b2.getFinalPrice() == 0 ? 0 : -1) );
+					b1.getSalePrice() -b2.getSalePrice() > 0 ? 1 : 
+						(b1.getSalePrice()-b2.getSalePrice() == 0 ? 0 : -1) );
 			bikes.sort(bikeSalePriceComparator.reversed()); //high to low
 			bikes.sort(bikeSalePriceComparator); //low to high
 
 			//sort by bike name
 			
-			//sort by date
+			//sort by register date
 			bikeSold.sort((Bike b1, Bike b2)->b1.getRegisterDate().compareTo(b2.getRegisterDate()));
-			bikeSold.forEach((bike)->System.out.println(bike.getBikeName()+" "+bike.getSoldDate()));
+			
+			//sort by soldDate
+			bikeSold.sort((Bike b1, Bike b2)->b1.getSoldDate().compareTo(b2.getSoldDate()));
+
+			//avg price of female/ male/ uni
+			int male = 0;
+			int female = 0;
+			int uni = 0;
+			for(Bike b: bikes) {
+				if(b.getGender()=="M") {
+					male++;
+				} else {
+					if(b.getGender()=="F") {
+						female++;
+					} else {
+						uni++;
+					}
+				}
+			}
+				
 			
 			
 		
 	} 
-	//sort by specific date range
-	public List<Bike> sortSoldBikesDate(LocalDateTime from, LocalDateTime to) throws DataAccessException{
-		BikeCtr bikeCtr = new BikeCtr();
-		List<Bike> result = new ArrayList<>();
-		List<Bike> bikes = bikeCtr.findAllBikes();
-		List<Bike> bikeSold = new ArrayList<>();
-		
-		for(Bike b: bikes) {
-			if(b.getSoldDate() != null) {
-				bikeSold.add(b);
-			}
-		}
-		for(Bike bs: bikeSold) {
-			if(bs.getSoldDate().compareTo(from) >= 0 && bs.getSoldDate().compareTo(to) <= 0 ) {
-				result.add(bs);}
-		}		
-		
-		return result;
 	
-}
 	
-	public List<Bike> sortAllBikesDate(LocalDateTime from, LocalDateTime to) throws DataAccessException{
-		BikeCtr bikeCtr = new BikeCtr();
-		List<Bike> bikes = bikeCtr.findAllBikes();
-		List<Bike> result = new ArrayList<>();
-		for(Bike bs: bikes) {
-			if(bs.getRegisterDate().compareTo(from) >= 0 && bs.getRegisterDate().compareTo(to) <= 0 ) {
-				result.add(bs);}
-		}		
-		
-		return result;
+
 	
-}
+	
 }
