37ed7c28fe851481884669d5902464bfb7f1b7d0
arsendr <minezspeletajs@gmail.com>
2020-05-27 15:44:06 +0200

    color class

--- src/model/Color.java
+++ src/model/Color.java
@@ -1,8 +1,36 @@
 package model;
+import java.awt.*;
 
 public class Color extends Object {
-    
-    public Color(int r, int g, int b) {
-        
+    public static Color BLACK;
+    public static Color GREEN;
+    public static Color RED;
+    public static Color PINK;
+    public static Color BLUE;
+    public static Color YELLOW;
+    public static Color colour;
+
+    public Color() {
+       
     }
+
+    public static Color getColour(String col) {
+
+        switch(col.toLowerCase()) {
+            case "black":
+                colour = Color.BLACK;
+            case "green":
+                colour = Color.GREEN;
+            case "red":
+                colour = Color.RED;
+            case "pink":
+                colour = Color.PINK;
+            case "blue":
+                colour = Color.BLUE;
+            case "yellow":
+                colour = Color.YELLOW;    
+        }
+        return colour;
+    }
+    
 }
